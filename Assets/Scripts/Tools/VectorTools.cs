ee9c7cd8e852720886c67b46c8448413ad9db9a6
Alexander Markov <alex.m.game.jam@gmail.com>
2023-11-24 15:45:26 +1000

    Update VectorTools.cs

--- Assets/Scripts/Tools/VectorTools.cs
+++ Assets/Scripts/Tools/VectorTools.cs
@@ -13,23 +13,32 @@ namespace Tools
     {
         public static Vector3 ToVector3(this Vector2 vector2, Vector2To3Type type = Vector2To3Type.XY)
         {
-            Vector3 vector3;
-            
             switch (type)
             {
                 case Vector2To3Type.XY:
-                    vector3 = new Vector3(vector2.x, vector2.y, 0);
-                    break;
+                    return new Vector3(vector2.x, vector2.y, 0);
                 
                 case Vector2To3Type.XZ:
-                    vector3 = new Vector3(vector2.x, 0, vector2.y);
-                    break;
+                    return new Vector3(vector2.x, 0, vector2.y);
                 
                 default:
                     throw new ArgumentException();
             }
+        }
 
-            return vector3;
+        public static Vector2 ToVector2(this Vector3 vector3, Vector2To3Type type = Vector2To3Type.XY)
+        {
+            switch (type)
+            {
+                case Vector2To3Type.XY:
+                    return new Vector2(vector3.x, vector3.y);
+                
+                case Vector2To3Type.XZ:
+                    return new Vector2(vector3.x, vector3.z);
+                
+                default:
+                    throw new ArgumentException();
+            }
         }
     }
 }
