274c4a76b1fea365d4a2b40dd4c3668a47cd4db3
AlexM4096 <alex.m.game.jam@gmail.com>
2023-12-25 00:40:59 +1000

    Ad

--- Assets/Scripts/CardSystem/CardData.cs
+++ Assets/Scripts/CardSystem/CardData.cs
@@ -6,7 +6,7 @@ namespace CardSystem
     public class CardData : ScriptableObject
     {
         [field: SerializeField] public int ID { get; private set; }
-        [field: SerializeField] public short Name { get; private set; }
+        [field: SerializeField] public string Name { get; private set; }
         [field: SerializeField] public CardCategory Category { get; private set; }
         [field: SerializeField] public Sprite Icon { get; private set; }
         [field: SerializeField] public int Description { get; private set; }
